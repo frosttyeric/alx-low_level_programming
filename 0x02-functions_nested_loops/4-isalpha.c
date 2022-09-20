fbdb9a18fbed4de9a47b14a28abfd6c79165b2b8
frosttyeric <frosttyeric>
2022-09-20 12:10:14 -0700

    Task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,12 @@
 
 /**
  * _isalpha - Shows 1 if the input is a
- *   * letter Another cases, shows 0
+ * letter Another cases, shows 0
  *
  * @c: The character in ASCII code
  *
-  * Return: 1 for letters. 0 for the rest.
+ * Return: 1 for letters. 0 for the rest.
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
